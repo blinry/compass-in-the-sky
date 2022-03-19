cb7fbea11f29f59c46c3d8eebeb2ba574afaa5cc
blinry <mail@blinry.org>
2022-03-19 16:29:10 +0100

    Add hour numbers

--- src/App.svelte
+++ src/App.svelte
@@ -9,18 +9,54 @@
     let pos
     let quizButtonText = "Quiz me!"
 
+    let date
     $: {
-        let date = new Date()
+        date = new Date()
         date.setHours(hour, 0, 0)
         date.setMonth(month - 1)
         pos = SunCalc.getPosition(date, 52, 10)
     }
 
     let offsetAzimuth = 0
-    $: sunAzimuth = offsetAzimuth + Math.PI
-    $: northAzimuth = offsetAzimuth - pos.azimuth
+    $: sunAzimuth = Math.PI + pos.azimuth
 
-    let m = {x: 0, y: 0}
+    let markers
+    $: {
+        markers = []
+
+        markers.push({label: "N", size: 1, radius: 1, azimuth: 0})
+        markers.push({label: "E", size: 1, radius: 1, azimuth: Math.PI / 2})
+        markers.push({label: "S", size: 1, radius: 1, azimuth: Math.PI})
+        markers.push({
+            label: "W",
+            size: 1,
+            radius: 1,
+            azimuth: (Math.PI * 3) / 2,
+        })
+
+        let times = SunCalc.getTimes(date, 52, 10)
+        let sunriseHour = times.sunrise.getHours()
+        let sunsetHour = times.sunset.getHours()
+
+        for (let hour2 = sunriseHour; hour2 <= sunsetHour; hour2 += 1) {
+            markers.push({
+                label: "" + hour2,
+                azimuth: -calcAzimuth(month, hour2) + calcAzimuth(month, hour),
+                size: 0.4,
+                radius: 0.8,
+            })
+        }
+
+        //markers.forEach((m) => (m.azimuth += offsetAzimuth))
+    }
+
+    function calcAzimuth(month, hour) {
+        let date = new Date()
+        date.setHours(hour, 0, 0)
+        date.setMonth(month - 1)
+        pos = SunCalc.getPosition(date, 52, 10)
+        return pos.azimuth
+    }
 
     function handleMousemove(event) {
         if (mousedown) {
@@ -29,7 +65,7 @@
                 event.clientY = event.touches[0].pageY
             }
             let loc = cursorPoint(event)
-            offsetAzimuth = Math.atan2(loc.y, loc.x)
+            offsetAzimuth = Math.atan2(loc.y, loc.x) - sunAzimuth + Math.PI
         }
     }
 
@@ -44,7 +80,7 @@
         if (showNorth) {
             offsetAzimuth = Math.random() * 2 * Math.PI
             hour = Math.floor(Math.random() * (21 - 6) + 6)
-            month = Math.floor(Math.random() * 12)
+            month = Math.floor(Math.random() * 12) + 1
             showNorth = false
             quizButtonText = "Reveal"
         } else {
@@ -77,7 +113,7 @@
 </script>
 
 <main>
-    Month: <input type="number" bind:value={month} min="0" max="11" />
+    Month: <input type="number" bind:value={month} min="1" max="12" />
     Hour: <input type="number" bind:value={hour} min="0" max="24" />
     <button on:click={reset}>Reset</button>
     <div
@@ -97,43 +133,58 @@
                 stroke="black"
                 stroke-width="0.005"
             />
+            {#if showNorth}
+                {#each markers as marker}
+                    <circle
+                        cx={0.4 *
+                            marker.radius *
+                            Math.cos(marker.azimuth + offsetAzimuth)}
+                        cy={0.4 *
+                            marker.radius *
+                            Math.sin(marker.azimuth + offsetAzimuth)}
+                        r="0.07"
+                        fill="white"
+                    />
+                {/each}
+            {/if}
             <circle
-                cx={0.4 * Math.cos(sunAzimuth)}
-                cy={0.4 * Math.sin(sunAzimuth)}
+                cx={0.3 * Math.cos(sunAzimuth + offsetAzimuth)}
+                cy={0.3 * Math.sin(sunAzimuth + offsetAzimuth)}
                 r="0.05"
                 fill="yellow"
                 stroke="black"
                 stroke-width="0.005"
             />
             {#if showNorth}
-                <circle
-                    cx={0.4 * Math.cos(northAzimuth)}
-                    cy={0.4 * Math.sin(northAzimuth)}
-                    r="0.07"
-                    fill="white"
-                />
-                <text
-                    text-anchor="middle"
-                    alignment-baseline="central"
-                    font-size="0.1"
-                >
-                    <tspan
-                        x={0.4 * Math.cos(northAzimuth)}
-                        y={0.4 * Math.sin(northAzimuth) + 0.03}>N</tspan
+                {#each markers as marker}
+                    <text
+                        text-anchor="middle"
+                        alignment-baseline="central"
+                        font-size={0.1 * marker.size}
                     >
-                </text>
+                        <tspan
+                            x={0.4 *
+                                marker.radius *
+                                Math.cos(marker.azimuth + offsetAzimuth)}
+                            y={0.4 *
+                                marker.radius *
+                                Math.sin(marker.azimuth + offsetAzimuth) +
+                                0.03}>{marker.label}</tspan
+                        >
+                    </text>
+                {/each}
             {/if}
             <line
                 x1="0"
                 y1="0"
-                x2={0.3 * Math.cos(sunAzimuth - Math.PI)}
-                y2={0.3 * Math.sin(sunAzimuth - Math.PI)}
+                x2={0.3 * Math.cos(sunAzimuth - Math.PI + offsetAzimuth)}
+                y2={0.3 * Math.sin(sunAzimuth - Math.PI + offsetAzimuth)}
                 stroke="black"
                 stroke-width="0.05"
             />
             <circle
-                cx={0.3 * Math.cos(sunAzimuth - Math.PI)}
-                cy={0.3 * Math.sin(sunAzimuth - Math.PI)}
+                cx={0.3 * Math.cos(sunAzimuth - Math.PI + offsetAzimuth)}
+                cy={0.3 * Math.sin(sunAzimuth - Math.PI + offsetAzimuth)}
                 r="0.025"
                 fill="black"
             />
@@ -148,6 +199,12 @@
         </svg>
     </div>
     <button on:click={startQuiz}>{quizButtonText}</button>
+    <br />
+    <p>
+        The numbers indicate where north it at that hour, relative to the
+        shadow.
+    </p>
+    <p />
 </main>
 
 <style>
